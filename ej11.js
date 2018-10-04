a48d10dea2163eef394eeafa82c0d1c34ab027d3
manuelczs <manuelczs@outlook.com>
2018-10-03 23:44:54 -0300

    Error 404 added

--- ej11.js
+++ ej11.js
@@ -8,4 +8,6 @@ app.get('/', (req, res) => res.sendFile(path.join(__dirname + '/index.html')));
 
 app.get('/productos.html', (req, res) => res.sendFile(path.join(__dirname + '/productos.html')));
 
+app.get(/\w/, (req, res) => res.sendFile(path.join(__dirname + '/error.html')))
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
